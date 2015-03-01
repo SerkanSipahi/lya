79e85fa972929004b1cf030ea5cef411a8ce52ce
Serkan Sipahi <serkan.sipahi@yahoo.de>
2015-03-01 13:56:34 +0100

    add todo for refactoring

--- lyadom.js
+++ lyadom.js
@@ -7,6 +7,20 @@
  * 
  */
 
+/**
+ * todo:
+ * - refactoring see below
+ *   HTMLElement.prototype[`${ns}addClass`] = function(...args){
+ *     return dom.addClass(this, ...args);
+ *   };
+ *   NodeList.prototype[`${ns}addClass`] =
+ *   HTMLCollection.prototype[`${ns}addClass`] = function(...args){
+ *      return dom.each(this, (_, domnode) => {
+ *          dom.addClass(domnode, ...args);
+ *      });
+ *   };
+ */
+
 import * as dom from 'lyadom/lib/domhelper';
 
 /**
