e448e7c85b1340d39775c17479c4b7539156a828
Serkan Sipahi <serkan.sipahi@yahoo.de>
2015-05-09 23:02:51 +0200

    tabs replaced by spaces

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,29 +1,28 @@
 module.exports = function(config) {
 
-	'use strict';
+    'use strict';
 
     config.set({
-		basePath: '../',
+        basePath: '../',
         files: [],
         singleRun: false,
-		port : 9876,
+        port : 9876,
         colors: true,
         plugins: [
-			'karma-systemjs',
-        	'karma-chrome-launcher',
-        	'karma-firefox-launcher',
-        	'karma-opera-launcher',
-        	'karma-safari-launcher',
-        	'karma-jasmine',
+            'karma-systemjs',
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-opera-launcher',
+            'karma-safari-launcher',
+            'karma-jasmine',
         ],
         frameworks: ['systemjs', 'jasmine'],
-        browsers: ['Chrome', 'Firefox', 'Safari', 'Opera', 'IE'],
-
+        browsers: ['Firefox', 'Chrome', 'Safari', 'Opera', 'IE'],
         systemjs: {
             configFile: 'lyadom.config.js',
             files: [
-            	'node_modules/rtts_assert/es6/src/rtts_assert.js',
-            	'lib/*.js',
+                'node_modules/rtts_assert/es6/src/rtts_assert.js',
+                'lib/*.js',
                 'test/*test.js',
             ],
             testFileSuffix: 'test.js'
