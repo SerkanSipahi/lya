6e6545a9b02a183fe1135e36be603675cd6c0107
Serkan Sipahi <serkan.sipahi@yahoo.de>
2015-05-10 21:02:57 +0200

    add systemjs text plugin for loading text files

--- config/system.config.js
+++ config/system.config.js
@@ -2,6 +2,7 @@ System.config({
     baseURL : './', 
     map: {
         assert: 'node_modules/rtts_assert/es6/src/rtts_assert',
+        text:   'node_modules/system-text/text'
     },
     traceurOptions : {
         types: true,
